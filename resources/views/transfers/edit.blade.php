78077bb646e44c04b00c56ec4e78682455cb93c6
Chinmay Purav <chinpurav@gmail.com>
2025-12-31 04:15:46 +0530

    chore: Refactor tag selection into a reusable component

--- resources/views/transfers/edit.blade.php
+++ resources/views/transfers/edit.blade.php
@@ -109,28 +109,7 @@
                     @enderror
                 </div>
 
-                @if($tags->count() > 0)
-                <div class="form-control mb-4">
-                    <label class="label">
-                        <span class="label-text">Tags (Optional)</span>
-                    </label>
-                    <div class="flex flex-wrap gap-2">
-                        @foreach ($tags as $tag)
-                            <label class="cursor-pointer flex items-center gap-2 px-3 py-1 rounded-full border border-base-300 hover:bg-base-200 transition-colors has-[:checked]:bg-primary/10 has-[:checked]:border-primary">
-                                <input type="checkbox" name="tags[]" value="{{ $tag->id }}"
-                                       class="checkbox checkbox-sm checkbox-primary"
-                                       {{ in_array($tag->id, old('tags', $transfer->tags->pluck('id')->toArray())) ? 'checked' : '' }}>
-                                <span class="text-sm" style="color: {{ $tag->color }}">{{ $tag->name }}</span>
-                            </label>
-                        @endforeach
-                    </div>
-                    @error('tags')
-                        <label class="label">
-                            <span class="label-text-alt text-error">{{ $message }}</span>
-                        </label>
-                    @enderror
-                </div>
-                @endif
+                <x-tag-selector :tags="$tags" :selected="$transfer->tags->pluck('id')->toArray()" />
 
                 <div class="flex justify-end gap-2 mt-6">
                     <a href="{{ route('transfers.index') }}" class="btn btn-ghost">Cancel</a>

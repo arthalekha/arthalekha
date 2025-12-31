ff2264873962e1fe94f32028d6e883b0814b8f03
Chinmay Purav <chinpurav@gmail.com>
2025-12-31 21:06:47 +0530

    chore: Add shortCode method to AccountType for concise representation

--- app/Enums/AccountType.php
+++ app/Enums/AccountType.php
@@ -11,4 +11,17 @@ enum AccountType: string
     case Investment = 'investment';
     case Loan = 'loan';
     case Other = 'other';
+
+    public function shortCode(): string
+    {
+        return match ($this) {
+            self::Cash => 'CA',
+            self::Bank => 'BK',
+            self::CreditCard => 'CC',
+            self::Wallet => 'WL',
+            self::Investment => 'IN',
+            self::Loan => 'LN',
+            self::Other => 'OT',
+        };
+    }
 }

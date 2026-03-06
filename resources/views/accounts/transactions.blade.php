dfc033f29b9a6f812378610aa4f4e3d73b10393e
Chinmay Purav <chinpurav@gmail.com>
2026-03-07 00:32:50 +0530

    feat: edit added in transactions directly

--- resources/views/accounts/transactions.blade.php
+++ resources/views/accounts/transactions.blade.php
@@ -112,6 +112,7 @@
                                 <th>Description</th>
                                 <th>Person / Account</th>
                                 <th class="text-right">Amount</th>
+                                <th></th>
                             </tr>
                         </thead>
                         <tbody>
@@ -151,6 +152,27 @@
                                             <span class="text-success">+{{ number_format($transaction['amount'], 2) }}</span>
                                         @else
                                             <span class="text-error">-{{ number_format($transaction['amount'], 2) }}</span>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if ($transaction['type'] === 'income')
+                                            <a href="{{ route('incomes.edit', $transaction['id']) }}" class="btn btn-ghost btn-xs">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                </svg>
+                                            </a>
+                                        @elseif ($transaction['type'] === 'expense')
+                                            <a href="{{ route('expenses.edit', $transaction['id']) }}" class="btn btn-ghost btn-xs">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                </svg>
+                                            </a>
+                                        @elseif (in_array($transaction['type'], ['transfer_in', 'transfer_out']))
+                                            <a href="{{ route('transfers.edit', $transaction['id']) }}" class="btn btn-ghost btn-xs">
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                </svg>
+                                            </a>
                                         @endif
                                     </td>
                                 </tr>

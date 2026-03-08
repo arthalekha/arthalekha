ea1ee5022d82e22bca86bfba5687c95504b69944
Chinmay Purav <chinpurav@gmail.com>
2026-03-08 14:51:01 +0530

    feat: use icons instead of text

--- resources/views/incomes/index.blade.php
+++ resources/views/incomes/index.blade.php
@@ -171,11 +171,16 @@
                                     </td>
                                     <td>
                                         <div class="flex justify-end gap-2">
-                                            <a href="{{ route('incomes.show', $income) }}" class="btn btn-ghost btn-sm">
-                                                View
+                                            <a href="{{ route('incomes.show', $income) }}" class="btn btn-ghost btn-sm tooltip" data-tip="View">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                                                </svg>
                                             </a>
-                                            <a href="{{ route('incomes.edit', $income) }}" class="btn btn-ghost btn-sm">
-                                                Edit
+                                            <a href="{{ route('incomes.edit', $income) }}" class="btn btn-ghost btn-sm tooltip" data-tip="Edit">
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-4 w-4">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
+                                                </svg>
                                             </a>
                                             <form action="{{ route('incomes.destroy', $income) }}" method="POST" class="inline"
                                                   onsubmit="return confirm('Are you sure you want to delete this income?');">
